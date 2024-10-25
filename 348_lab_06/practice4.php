ef47184f3d04830614256faf0765fb10591d9e0b
ZachMcCauley <142922513+ZachMcCauley@users.noreply.github.com>
2024-10-25 12:47:00 -0500

    Update practice4.php

--- 348_lab_06/practice4.php
+++ 348_lab_06/practice4.php
@@ -1,9 +1,27 @@
 <?php
-	$i = 5;
-	for ($i = 0; $i < size; $i++) {
-		for ($j = 0; $j < size; $j++) {
-			print $i * $j;
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$size = intval($_POST["size"]);
+		if ($size > 0) {
+			print "<h2>Multiplication Table of size $size</h2>";
+			print "<table border='1' cellpadding='5' cellspacing='0'>";
+
+			print "<tr<td></td>";
+			for ($i = 1; $i <= $size; $i++) {
+				print "<td><strong>$i</strong></td>";
+			}
+			print "</tr>";
+
+			for ($i = 1; $i <= $size; $i++) {
+				print "<tr>";
+				print "<td><strong>$i</strong></td>";
+				for ($j = 1; $j <= $size; $j++) {
+					print "<td>" . ($i * $j) . "</td>";
+				}
+				print "</tr>";
+			}
+			print "</table>";
+		} else {
+			print "Please enter a valid positive number.";
 		}
-		print ".\n";
 	}
 ?>
